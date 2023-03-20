ecdcc6e32a4ada7aba9034c800fcc4475720a396
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-19 23:17:24 -0400

    Create Header.js and move 'import Navigation' from App.js to Header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./components/Header";
-import Navigation from "./components/Navigation"; // SHOULD BE IN HEADER
 import Project from "./components/Project";
 import Footer from "./components/Footer";
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from "./Navigation"; // SHOULD BE IN HEADER
+// Here we import the Header.css file to grant access to some additional classNames
+import '../styles/Header.css';
+
+// TODO: Create a styles object called "styles"
+// const styles = header;
+
+function Header() {
+  // TODO: Add a style attribute to header and the h1 element
+
+  return (
+    <header className="header">
+      <h1>Welcome</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
