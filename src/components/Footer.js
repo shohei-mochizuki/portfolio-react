5ca99c649c6fb53e1d5d5edfa0967589fee1ad81
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-20 22:53:33 -0400

    Fix image rendering issue in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import linkedinLogo from '../images/linkedin.png';
+import githubLogo from '../images/github.svg';
+import twitterLogo from '../images/twitter.svg';
 
 export default function Footer() {
   return (
     <div>
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a href="https://www.linkedin.com/in/shoheimochizuki"><img src="logos/linkedin.png" alt="LinkedIn logo"/></a>
+          <a href="https://www.linkedin.com/in/shoheimochizuki"><img src={linkedinLogo} alt="LinkedIn logo" height={"40px"}/></a>
         </li>
         <li className="nav-item">
-          <a href="https://github.com/shohei-mochizuki"><img src="logos/github.svg" alt="GitHub logo"/></a>
+          <a href="https://github.com/shohei-mochizuki"><img src={githubLogo} alt="GitHub logo" height={"40px"}/></a>
         </li>
         <li className="nav-item">
-          <a href="https://twitter.com/ShoheiYYZ"><img src="logos/twitter.svg" alt="Twitter logo"/></a>
+          <a href="https://twitter.com/ShoheiYYZ"><img src={twitterLogo} alt="Twitter logo" height={"40px"}/></a>
         </li>
       </ul>
     </div>
