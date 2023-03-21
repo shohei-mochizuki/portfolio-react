c005bd2c3fb8d7bbc7454e127c2ca08caa617c7f
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-20 22:11:32 -0400

    Create PortfolioContainer and delete unnecessary pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
-import Project from "./components/Project";
-import Footer from "./components/Footer";
+import PortfolioContainer from "./components/PortfolioContainer";
 
 
 function App() {
   return (
     <div>
-      <Header />
-      <Project />
-      <Footer />
+      <PortfolioContainer />
     </div>
   );
 }
