82a0b41ff7ff8f18a1c5656ae4744195c194d8f8
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-22 18:06:25 -0400

    Add Jupyter and Tableau to skill section in Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -16,10 +16,12 @@ import logoGit from '../../images/git.svg';
 import logoGitHub from '../../images/github.svg';
 import logoVsCode from '../../images/vscode.svg';
 import logoPwa from '../../images/pwa.png';
-import logoPython from '../../images/python.svg';
+import logoPython from '../../images/python.png'
+import logoJupyter from '../../images/jupyter.png'
 import logoR from '../../images/r.png';
 import logoBigQuery from '../../images/bigquery.png';
 import logoDataStudio from '../../images/datastudio.svg';
+import logoTableau from '../../images/tableau.png';
 
 export default function Resume() {
   return (
@@ -28,96 +30,109 @@ export default function Resume() {
       <h3>Resume</h3>      
       <button><a href={resume}>Resume</a></button>
       <h3>Coding skills</h3>
-        <div className="m-5 p-5 bg-light rounded-5">
-          <div className="container text-center">
-            <div className="row row-cols-lg-4 row-cols-3">
-              <div className="col my-4">
-                <img src={logoHtml} width="50" height="50" />
-                <p>HTML</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoCss} width="50" height="50" />
-                <p>CSS</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoBootstrap} width="60" height="50" />
-                <p>Bootstrap</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoJavascript} width="50" height="50" />
-                <p>JavaScript</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoJquery} width="50" height="50" />
-                <p>JQuery</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoNode} width="70" height="50" />
-                <p>Node</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoReact} width="50" height="50" />
-                <p>React</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoApi} width="50" height="50" />
-                <p>REST API</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoHandlebars} width="50" height="50" />
-                <p>Handlebars</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoMySql} width="70" height="50" />
-                <p>MySQL</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoMongoDb} width="50" height="50" />
-                <p>MongoDB</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoGas} width="50" height="50" />
-                <p>Google App Script</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoGit} width="50" height="50" />
-                <p>Git</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoGitHub} width="50" height="50" />
-                <p>GitHub</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoVsCode} width="50" height="50" />
-                <p>VS Code</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoPwa} width="70" height="50" />
-                <p>PWA</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoPython} width="50" height="50" />
-                <p>Python</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoR} width="50" height="50" />
-                <p>R</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoBigQuery} width="50" height="50" />
-                <p>BigQuery</p>
-              </div>
-              <div className="col my-4">
-                <img src={logoDataStudio} width="50" height="50" />
-                <p>DataStudio</p>
-              </div>
+      <div className="m-5 p-5 bg-light rounded-5">
+        <div className="container text-center">
+          <div className="row row-cols-lg-4 row-cols-3">
+            <div className="col my-4">
+              <img src={logoHtml} width="50" height="50" />
+              <p>HTML</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoCss} width="50" height="50" />
+              <p>CSS</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoBootstrap} width="60" height="50" />
+              <p>Bootstrap</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoJavascript} width="50" height="50" />
+              <p>JavaScript</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoJquery} width="50" height="50" />
+              <p>JQuery</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoNode} width="70" height="50" />
+              <p>Node</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoReact} width="50" height="50" />
+              <p>React</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoApi} width="50" height="50" />
+              <p>REST API</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoHandlebars} width="50" height="50" />
+              <p>Handlebars</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoMySql} width="70" height="50" />
+              <p>MySQL</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoMongoDb} width="50" height="50" />
+              <p>MongoDB</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoGas} width="50" height="50" />
+              <p>Google App Script</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoGit} width="50" height="50" />
+              <p>Git</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoGitHub} width="50" height="50" />
+              <p>GitHub</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoVsCode} width="50" height="50" />
+              <p>VS Code</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoPwa} width="70" height="50" />
+              <p>PWA</p>
             </div>
           </div>
         </div>
-      <h3>Data analytics skills</h3>
-      <div>
-        
       </div>
+      <h3>Data analytics skills</h3>
+      <div className="m-5 p-5 bg-light rounded-5">
+        <div className="container text-center">
+          <div className="row row-cols-lg-4 row-cols-3">
+            <div className="col my-4">
+              <img src={logoPython} width="50" height="50" />
+              <p>Python</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoJupyter} width="40" height="50" />
+              <p>Jupyter</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoR} width="50" height="50" />
+              <p>R</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoBigQuery} width="50" height="50" />
+              <p>BigQuery</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoDataStudio} width="50" height="50" />
+              <p>DataStudio</p>
+            </div>
+            <div className="col my-4">
+              <img src={logoTableau} width="50" height="50" />
+              <p>Tableau</p>
+            </div>
+          </div>
+        </div>
+      </div>
+        
+      
       <h3>Other skills</h3>
       <div>
         <ul>
