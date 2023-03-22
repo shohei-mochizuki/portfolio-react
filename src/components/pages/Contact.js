f99487c5648f6495808dc295d18ed2205ac81901
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-21 23:54:37 -0400

    Add functionalities including validations to Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 // Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../../utils/helpers';
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
   // Create state variables for the fields in the form
@@ -9,7 +9,10 @@ export default function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [disableButton, setDisableButton] = useState(true);
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
@@ -24,35 +27,47 @@ export default function Contact() {
       setEmail(inputValue);
     } else {
       setMessage(inputValue);
+    };
+  };
+
+  const validation = (e) => {
+    const { target } = e; // <==> const target = e.target
+    const inputType = target.name;
+
+    if (inputType === 'name'){
+      if (!name) {
+        setErrorName('Name is required');
+      } else {
+        setErrorName('');
+      }
+    } else if (inputType === 'email') {
+        if (!validateEmail(email)) {
+          setErrorEmail('Email is invalid');
+        } else {
+          setErrorEmail('');
+        }
+    } else {
+      if (!message) {
+        setErrorMessage('Message is required');
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (name!=="" && validateEmail(email) && message!=="") {
+      setDisableButton(false);
+    } else {
+      setDisableButton(true);
     }
   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!name){
-      setErrorMessage('Name is empty');   
-      return;  
-    }
-    
-    if (!validateEmail(email)) {
-      setErrorMessage('Email is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-
-    if (!message) {
-      setErrorMessage('Name is empty');
-      return;
-    }
-
+const handleFormSubmit = (e) => { 
+  e.preventDefault();
     // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setName('');
-    setEmail('');
-    setMessage('');
+  setName('');
+  setEmail('');
+  setMessage('');
   };
 
   return (
@@ -63,6 +78,7 @@ export default function Contact() {
           value={name}
           name="name"
           onChange={handleInputChange}
+          onBlur={validation}
           type="text"
           placeholder="Enter your name"
         />
@@ -70,6 +86,7 @@ export default function Contact() {
           value={email}
           name="email"
           onChange={handleInputChange}
+          onBlur={validation}
           type="email"
           placeholder="Enter your email"
         />
@@ -77,11 +94,22 @@ export default function Contact() {
           value={message}
           name="message"
           onChange={handleInputChange}
+          onBlur={validation}
           type="text"
           placeholder="Enter your message"
         />
-        <button type="button" onClick={handleFormSubmit}>Send</button>
+        <button type="button" onClick={handleFormSubmit} disabled={disableButton}>Send</button>
       </form>
+      {errorName && (
+        <div>
+          <p className="error-text">{errorName}</p>
+        </div>
+      )}
+      {errorEmail && (
+        <div>
+          <p className="error-text">{errorEmail}</p>
+        </div>
+      )}
       {errorMessage && (
         <div>
           <p className="error-text">{errorMessage}</p>
