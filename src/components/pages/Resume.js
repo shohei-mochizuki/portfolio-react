5f93928cd0032417811e70eb2a5c15fedc677ec9
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-21 23:55:29 -0400

    Create Resume.js

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import facePhoto from '../../images/shohei.jpg';
+
+export default function Resume() {
+  return (
+    <div>
+      <img src={facePhoto} alt="Shohei" height={"200px"}/>
+      <h5>Hello World 🌎 ! I'm</h5>
+      <h2>Shohei Mochizuki</h2>
+      <p><small >Pronunciation: show-hay moe-chee-zoo-key | Pronuous: he/his/him</small></p>
+      <p>A full stack web developer with 8 years of working experience in data analytics and engineering fields. Love solving issues with other people and working in a diverse environment.</p>
+    </div>
+  );
+}
