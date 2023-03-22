6a353c46f5ef0dd5eaf28dcb2ae825c884710a9a
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-21 22:23:51 -0400

    Create Project.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Project from './Project';
-// import githubLogo from '../../images/github.svg';
-// import pjTravelerReview from '../../images/pj15-traveler-review.jpg';
 
 export default function Portfolio() {
   
@@ -56,12 +54,3 @@ export default function Portfolio() {
     </div>
   );
 }
-
-        // <div>
-        //   <Project/>
-        //     <img src={pjTravelerReview} alt="LinkedIn logo" height={"300px"}/>
-        //     <a href="https://www.yahoo.co.jp/">Traveler's Choice</a>
-        //     <a href="https://github.com/shohei-mochizuki/TravelersChoice">
-        //       <img src={githubLogo} alt="GitHub logo" height={"40px"}/>
-        //     </a>
-        // </div>
---
+++ src/components/pages/Project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import facePhoto from '../../images/shohei.jpg';
+
+export default function Project() {
+  return (
+    <div>
+        <img src={pjTravelerReview} alt="LinkedIn logo" height={"300px"}/>
+        <a href="https://www.yahoo.co.jp/">Traveler's Choice</a>
+        <a href="https://github.com/shohei-mochizuki/TravelersChoice">
+          <img src={githubLogo} alt="GitHub logo" height={"40px"}/>
+        </a>
+    </div>
+  );
+}
