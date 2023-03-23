2e9a95220b47db33ea938d192a6b9841bfc7c304
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-22 20:59:25 -0400

    Update styles in Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,11 +3,12 @@ import githubLogo from "../../images/github.svg"
 
 export default function Project({imgSrc, imgAlt, hrefDeploy, hrefGitHub}) {
   return (
-    <div>
-      <img src={imgSrc} alt={imgAlt} height={"300px"}/>
-      <a href={hrefDeploy}>Traveler's Choice</a>
-      <a href={hrefGitHub}>
+    <div className="position-relative">
+      <img src={imgSrc} alt={imgAlt} height={"300px"} className="opacity-50" />
+      <a href={hrefDeploy} className="position-absolute top-0 start-50 translate-middle-x fs-3 text-decoration-none text-black mt-5">Deployed site</a>
+      <a href={hrefGitHub} className="position-absolute bottom-0 start-50 translate-middle-x fs-5 text-decoration-none text-black mb-5">
         <img src={githubLogo} alt="GitHub logo" height={"40px"}/>
+        <p> GitHub Repo</p>
       </a>
     </div>
   );
