61927778afd52fa384a0cad9e4070407e511303d
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-22 22:10:49 -0400

    Fix header link issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ export default function Header({ currentPage, handlePageChange }) {
     <div>
       <nav className="navbar navbar-expand-md bg-warning px-3">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#aboutme">
+          <div className="navbar-brand">
             <img src={logoSite} width="30" height="30" className="d-inline-block align-text-bottom" alt="" />
             <span className="fs-3 fw-bold ms-2 text-white">Shohei Mochizuki</span>
-          </a>
+          </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
