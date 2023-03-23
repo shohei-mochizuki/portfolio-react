21211cff13affe83fbbf42d611c4381cf6798844
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-22 21:29:09 -0400

    Add work details to Portfolio.js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import Project from './Project';
-import a from "../../images/pj15-traveler-review.jpg";
+import pj19 from "../../images/pj19-texteditor-pwa.png";
+import pj15 from "../../images/pj15-traveler-review.jpg";
+import pj11 from "../../images/pj11-notetaker.png";
+import pj9 from "../../images/pj9-tenthings-toronto.jpg";
+import pj6 from "../../images/pj6-weather-dashboard.jpg";
+import pj4 from "../../images/pj4-interactive-quiz.png";
 
 export default function Portfolio() {
   
   const projects = [
     {
-      title: a,
-      imgSrc: a,
+      title: "Text Editor - Progressive Web Application",
+      imgSrc: pj19,
+      imgAlt: "Text Editor image",
+      hrefDeploy: "https://text-editor-pwa-2023.herokuapp.com/",
+      hrefGitHub: "https://github.com/shohei-mochizuki/text-editor-pwa",
+    },
+    {
+      title: "Traveler's Choice",
+      imgSrc: pj15,
       imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
+      hrefDeploy: "https://travelerschoice.herokuapp.com/",
       hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
     },
     {
-      title: a,
-      imgSrc: a,
-      imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
-      hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
+      title: "Easy Note Taker",
+      imgSrc: pj11,
+      imgAlt: "Easy Note Taker image",
+      hrefDeploy: "https://shohei-easynotetaker.herokuapp.com/",
+      hrefGitHub: "https://github.com/shohei-mochizuki/challenge11-expressjs-easynotetaker",
     },
     {
-      title: a,
-      imgSrc: a,
-      imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
-      hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
+      title: "10 Things to Do in Toronto",
+      imgSrc: pj9,
+      imgAlt: "10 Things to Do in Toronto image",
+      hrefDeploy: "https://shohei-mochizuki.github.io/10things-todo-in-toronto/",
+      hrefGitHub: "https://github.com/shohei-mochizuki/10things-todo-in-toronto",
     },
     {
-      title: a,
-      imgSrc: a,
-      imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
-      hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
+      title: "World Weather Dashboard",
+      imgSrc: pj6,
+      imgAlt: "World Weather Dashboard image",
+      hrefDeploy: "https://shohei-mochizuki.github.io/challenge06-world-weather-dashboard/",
+      hrefGitHub: "https://github.com/shohei-mochizuki/challenge06-world-weather-dashboard",
     },
     {
-      title: a,
-      imgSrc: a,
-      imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
-      hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
-    },
-    {
-      title: a,
-      imgSrc: a,
-      imgAlt: "Traveler's choice image",
-      hrefDeploy: "https://github.com/shohei-mochizuki/TravelersChoice",
-      hrefGitHub: "https://github.com/shohei-mochizuki/TravelersChoice",
+      title: "Coding Quiz Challenge",
+      imgSrc: pj4,
+      imgAlt: "Coding Quiz Challenge image",
+      hrefDeploy: "https://shohei-mochizuki.github.io/challenge04-interactive-quiz/",
+      hrefGitHub: "https://github.com/shohei-mochizuki/challenge04-interactive-quiz",
     }];
   
   return (
